c1643106e591dd647857577d8e2167611020de51
LZ <1758648709@qq.com>
2019-10-15 11:13:27 +0800

    OSS上传

---
+++ OSS/OSSRight.java
@@ -0,0 +1,15 @@
+package com.up.utils;
+
+public class LzOosBi {
+    //阿里云API的外网域名
+    public static final String ENDPOINT = "oss-cn-beijing.aliyuncs.com";
+    //阿里云API的密钥Access Key ID
+    public static final String ACCESS_KEY_ID = "LTAIdwxIOKhrVmAJ";
+    //阿里云API的密钥Access Key Secret
+    public static final String ACCESS_KEY_SECRET = "wF6JfwLlHt5MH37kjD9XR3BBIvPtpA";
+    //阿里云API的bucket名称
+    public static final String BACKET_NAME = "perserve";
+    //阿里云API的文件夹名称
+    public static final String FOLDER="photo/";
+
+}
